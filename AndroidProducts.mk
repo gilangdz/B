0fede4b6cd1476e38fe49bb8dffe229ad2505093
gilangdz <gilangdzakky15@gmail.com>
2023-12-22 22:59:31 +0700

    First Commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X695C.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X695C-user \
+    twrp_X695C-userdebug \
+    twrp_X695C-eng
